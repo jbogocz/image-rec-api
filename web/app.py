d98fdabb72ffc863916d4d61cc14d67242fc29ec
Jacek Bogocz <jacek.bogocz@gmail.com>
2020-05-10 18:22:19 +0200

    add api resources

--- web/app.py
+++ web/app.py
@@ -174,3 +174,10 @@ class Refill(Resource):
         return jsonify(generateReturnDictionary(200, 'Refilled Successfully'))
 
 
+# add resources to the API
+api.add_resource(Register, '/register')
+api.add_resource(Classify, '/classify')
+api.add_resource(Refill, '/refill')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
