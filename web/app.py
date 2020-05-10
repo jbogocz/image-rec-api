16a5d1c91bcf24e02500a780855da52ac880ad01
Jacek Bogocz <jacek.bogocz@gmail.com>
2020-05-10 13:52:51 +0200

    import required libs

---
+++ web/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+import requests
+import subprocess
+import json
+import bcrypt
+# import numpy as np
+import tensorflow as tf
+
